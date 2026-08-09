192bc96d704275b4d2f92d4784b853ebda469f34
konstantin-paulus <mail@konstantinpaulus.de>
2026-08-09 23:17:11 +0200

    Update DMG background assets and adjust file positions in configuration
    Modified the `dmg-background.svg` file to enhance the visual design and updated the associated PNG files for consistency. Adjusted the file positions in the `forge.config.ts` to improve layout within the application window. These changes aim to enhance the user experience during application installation.

--- apps/desktop/forge.config.ts
+++ apps/desktop/forge.config.ts
@@ -52,8 +52,8 @@ const config: ForgeConfig = {
         window: { size: { width: 658, height: 498 } },
       },
       contents: (opts) => [
-        { x: 188, y: 344, type: 'file', path: opts.appPath },
-        { x: 470, y: 344, type: 'link', path: '/Applications' },
+        { x: 188, y: 217, type: 'file', path: opts.appPath },
+        { x: 470, y: 217, type: 'link', path: '/Applications' },
       ],
     }),
   ],
