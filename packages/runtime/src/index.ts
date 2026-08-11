a28dbfaebbbd6047d91d55b8536bcaa3d2f773f2
konstantin-paulus <mail@konstantinpaulus.de>
2026-08-11 18:29:53 +0200

    Add world serialization functionality to runtime package
    Introduced a new module for world serialization in the runtime package, including the `serializeEntity` function and the `EntityRecord` interface. Updated the index file to export the new serialization module, enhancing the capabilities for entity management and persistence in the application.

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,6 +1,7 @@
 export * from './constants';
 export * from './traits';
 export * from './world/create-world';
+export * from './world/serialize';
 export * from './capture';
 export * from './fonts';
 export * from './utils';
